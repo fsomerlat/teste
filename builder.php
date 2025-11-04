86869c8f3b996a522a2b1cb3db6636e52e30753f
Fs <fsomerlat@gmail.com>
2025-11-04 17:22:14 -0300

    add var teste4

--- builder.php
+++ builder.php
@@ -18,7 +18,8 @@ class UserBuilder
         $this->usuario = new User();
         $this->teste = 'teste';
         $this->teste2 = 'teste2';
-        $this->teste3 = 'teste3';   
+        $this->teste3 = 'teste3';
+        $this->teste3 = 'teste4';    
 
     }
 
