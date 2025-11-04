287a37f446a59704e4d7032204e9bd40219b9a03
Fs <fsomerlat@gmail.com>
2025-11-04 16:57:26 -0300

    teste2

--- builder.php
+++ builder.php
@@ -17,6 +17,7 @@ class UserBuilder
     {
         $this->usuario = new User();
         $this->teste = 'teste';
+        $this->teste2 = 'teste2';
     }
 
     public function setName(string $name): self
